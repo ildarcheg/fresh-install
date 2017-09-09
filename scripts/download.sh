70a07e2fad920094e2718c2c7faabbf779f8150a
Ildar Gabdrakhmanov <ildarcheg@gmail.com>
2017-09-09 11:48:52 +0700

    download script updated

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,28 @@
 # Download 1C Fresh sources
 
-sudo apt-get install curl
-curl -O http://61.28.226.190/fresh-install-64.zip
-unzip fresh-install-64.zip
+# Echo message
+function message() {
+	delimiter
+	echo $1
+	delimiter
+}
+
+message "install Curl"
+
+sudo apt-get  --yes --force-yes install curl
+
+message "download sources"
+
+sudo curl -O http://61.28.226.190/fresh-install-64.zip
+sudo unzip fresh-install-64.zip
+
+message "copy sources to /fresh-install"
+
 sudo cp -a fresh-install-64/* /fresh-install/
+
+message "delete sources"
+
 rm -fr fresh-install-64
 rm fresh-install-64.zip
+
+message "ALL DONE"
